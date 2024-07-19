1c661394c972f1b1b2f6e78df95c7258937f8e33
wes23dev <137829604+wes23dev@users.noreply.github.com>
2024-07-19 08:46:20 -0300

    Add files via upload

---
+++ ProdutoHeranca/src/main/java/Classes/Eletronico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author 15102938681
+ */
+public class Eletronico extends Produto {
+    private int potencia;
+    
+    
+    
+}
---
+++ ProdutoHeranca/src/main/java/com/mycompany/produtoheranca/ProdutoHeranca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.produtoheranca;
+
+/**
+ *
+ * @author 15102938681
+ */
+public class ProdutoHeranca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
